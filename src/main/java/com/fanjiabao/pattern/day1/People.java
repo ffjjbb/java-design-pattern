41360f44b5d7c74bdcaf28604520ddb740c4fca3
FanJiaBao <18309580634@163.com>
2026-04-02 10:29:48 +0800

    项目初始化

---
+++ src/main/java/com/fanjiabao/pattern/day1/People.java
@@ -0,0 +1,12 @@
+package com.fanjiabao.pattern.day1;
+
+/**
+ * @author: FanJiaBao
+ * @createDate: 2026/4/2 10:28
+ * @description: 描述
+ */
+public class People {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
